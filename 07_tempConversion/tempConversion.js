192d02ec53ab6c1cfb3bde1604241759a9e2f3ae
nicocro <ncroce00@gmail.com>
2024-02-18 17:11:05 +0000

    07 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  return Number(((5/9) * (fahr - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return Number(((9/5) * cels + 32).toFixed(1));
 };
 
 // Do not edit below this line
