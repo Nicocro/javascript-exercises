017d1d5e193f438ba42eaa34b75ae772725c5317
nicocro <ncroce00@gmail.com>
2024-02-18 16:43:26 +0000

    05 solved elegantly I would say

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number"){
+        return "ERROR"
+    }
+    if (a < 0 || b < 0){
+        return "ERROR"
+    }
+    const min = Math.min(a,b);
+    const max = Math.max(a,b);
+    return Math.abs( ((max*(max+1))/2) - ((min*(min+1))/2) + min)
 };
 
 // Do not edit below this line
