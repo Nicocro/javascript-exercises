8ce1b2bea5dcaa77d8caf2f3e3d1c99bf5df2d4d
nicocro <ncroce00@gmail.com>
2024-02-18 17:03:23 +0000

    05 solved better

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -5,9 +5,10 @@ const sumAll = function(a, b) {
     if (a < 0 || b < 0){
         return "ERROR"
     }
-    const min = Math.min(a,b);
-    const max = Math.max(a,b);
-    return Math.abs( ((max*(max+1))/2) - ((min*(min+1))/2) + min)
+    // const min = Math.min(a,b);
+    // const max = Math.max(a,b);
+    // return Math.abs( ((max*(max+1))/2) - ((min*(min+1))/2) + min)
+    return (Math.abs( ((a*(a+1))/2) - ((b*(b+1))/2) ) + Math.min(a,b));
 };
 
 // Do not edit below this line
