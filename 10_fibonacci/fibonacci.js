b977dc58e3113ea7cfc26cecb4ac08493edb3faf
nicocro <ncroce00@gmail.com>
2024-03-09 09:16:57 +0000

    10 solved, there are conflicting tests though

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fibo = [1, 1];
+    for (let i=2; i < n; i++) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
+    }
+    return fibo[n-1];
 };
 
 // Do not edit below this line
