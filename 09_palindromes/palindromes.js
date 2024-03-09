581bba264baaa1c30da14c64cbcaa6115ddee560
nicocro <ncroce00@gmail.com>
2024-03-09 08:51:07 +0000

    9 is solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (stringa) {
+    let cleanedString = stringa.toLowerCase().replace(/[\W_]/g, '');
+    let reversed = cleanedString.split('').reverse().join('');
+    return cleanedString === reversed;
 };
 
 // Do not edit below this line
